9e1759a99f5b16aee1aae1ae961d6ee8ba544a8c
wandypurnomo <wandypurnomo92@gmail.com>
2019-08-08 16:43:15 +0700

    initial commit

---
+++ src/Popo/Menu.php
@@ -0,0 +1,156 @@
+<?php namespace Wandx\Modularity\Popo;
+
+use Eventy;
+
+class Menu
+{
+    public $id;
+    public $name;
+    public $icon;
+    public $visible;
+    public $active;
+    public $url;
+
+    public function __construct(string $id = null, string $name = null, string $icon = null, bool $visible = false, bool $active = false, string $url = null)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->icon = $icon;
+        $this->visible = $visible;
+        $this->active = $active;
+        $this->url = $url;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "icon" => $this->icon,
+            "visible" => $this->visible,
+            "active" => $this->active,
+            "url" => $this->url,
+        ];
+    }
+
+    public function addMenu(): Menu
+    {
+        Eventy::addFilter("admin-menu", function ($old) {
+            if ($old == "") {
+                $old = [];
+            }
+
+            $old[] = $this->toArray();
+            return $old;
+        }, 20, 1);
+
+        return $this;
+    }
+
+    public function subMenuOf(string $parentId): Menu
+    {
+        Eventy::addFilter("admin-menu", function ($old) use ($parentId) {
+            $all = $old;
+
+            foreach ($all as $k => $v) {
+                if ($all[$k]["id"] == $parentId) {
+                    $all[$k]["children"][] = $this->toArray();
+                }
+
+                if ($this->toArray()["active"]) {
+                    $all[$k]["active"] = true;
+                }
+            }
+
+            return $all;
+        }, 20, 1);
+
+        return $this;
+    }
+
+    public static function setActive($id)
+    {
+        $allMenuReference = Eventy::filter("admin-menu");
+        $container = [];
+        foreach ($allMenuReference as $k => $menu) {
+            $container[$k] = $menu;
+
+            if ($container[$k]["id"] == $id) {
+                $container[$k]["active"] = true;
+            }
+
+            if (isset($container[$k]["children"])) {
+                foreach ($container[$k]["children"] as $k2 => $children) {
+                    if ($id == $container[$k]["children"][$k2]["id"]) {
+                        $container[$k]["active"] = true;
+                        $container[$k]["children"][$k2]["active"] = true;
+                    }
+                }
+            }
+        }
+
+        Eventy::addFilter("admin-menu", function ($old) use ($container) {
+            return $container;
+        });
+    }
+
+    public static function setVisible(array $ids)
+    {
+        $allMenuReference = Eventy::filter("admin-menu");
+        $container = [];
+        foreach ($allMenuReference as $k => $menu) {
+            $container[$k] = $menu;
+
+            if (in_array($container[$k]["id"], $ids)) {
+                $container[$k]["visible"] = true;
+            }
+
+            if (isset($container[$k]["children"])) {
+                foreach ($container[$k]["children"] as $k2 => $children) {
+                    if (in_array($container[$k]["children"][$k2]["id"], $ids)) {
+                        $container[$k]["visible"] = true;
+                        $container[$k]["children"][$k2]["visible"] = true;
+                    }
+                }
+            }
+        }
+
+        Eventy::addFilter("admin-menu", function ($old) use ($container) {
+            return $container;
+        });
+    }
+
+    public static function setInvisible(array $ids)
+    {
+        $allMenuReference = Eventy::filter("admin-menu");
+        $container = [];
+        foreach ($allMenuReference as $k => $menu) {
+            $container[$k] = $menu;
+
+            if (in_array($container[$k]["id"], $ids)) {
+                $container[$k]["visible"] = false;
+            }
+
+            if (isset($container[$k]["children"])) {
+                if ($container[$k]["visible"]) {
+                    foreach ($container[$k]["children"] as $k2 => $children) {
+                        if (in_array($container[$k]["children"][$k2]["id"], $ids)) {
+                            $container[$k]["visible"] = false;
+                            $container[$k]["children"][$k2]["visible"] = false;
+                        }
+                    }
+                } else {
+                    foreach ($container[$k]["children"] as $k2 => $children) {
+                        $container[$k]["visible"] = false;
+                        $container[$k]["children"][$k2]["visible"] = false;
+                    }
+                }
+
+            }
+        }
+
+        Eventy::addFilter("admin-menu", function ($old) use ($container) {
+            return $container;
+        });
+    }
+}
---
+++ src/Providers/ModularityProvider.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Wandx\Modularity\Providers;
+
+use Eventy;
+use File;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Nadar\PhpComposerReader\Autoload;
+use Nadar\PhpComposerReader\AutoloadSection;
+use Nadar\PhpComposerReader\ComposerReader;
+
+class ModularityProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->updateComposerAutoload();
+        $this->discoverMeta();
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    private function discoverMeta()
+    {
+        // containers
+        $providers = [];
+        $aliases = [];
+        $migrationPaths = [];
+        $metas = [];
+
+        $base = base_path("packages");
+        $exclude = [".", ".."];
+        try{
+            $dirs = scandir($base);
+            foreach ($dirs as $dir) {
+                if (!in_array($dir, $exclude)) {
+                    $subDirs = scandir($base . DIRECTORY_SEPARATOR . $dir);
+                    foreach ($subDirs as $subdir) {
+                        if (!in_array($subdir, $exclude)) {
+                            $packageDir = $base . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $subdir;
+                            $srcDir = $packageDir . DIRECTORY_SEPARATOR . "src";
+                            $meta = include $packageDir . DIRECTORY_SEPARATOR . "meta.php";
+
+                            if (is_dir($packageDir . DIRECTORY_SEPARATOR . "resources/views")) {
+                                $meta["viewPath"] = $packageDir . DIRECTORY_SEPARATOR . "resources/views";
+
+                                // set view namespace
+                                $this->app["view"]->addNamespace($subdir, $meta["viewPath"]);
+                            }
+
+                            // scan helper
+                            if (file_exists($srcDir . DIRECTORY_SEPARATOR . "helper.php")) {
+                                include $srcDir . DIRECTORY_SEPARATOR . "helper.php";
+                            }
+
+                            // scan routes
+                            if (file_exists($srcDir . DIRECTORY_SEPARATOR . "routes.php")) {
+                                include $srcDir . DIRECTORY_SEPARATOR . "routes.php";
+                            }
+
+                            // scan assets
+                            if (is_dir($packageDir . DIRECTORY_SEPARATOR . "assets") && !is_dir(public_path("vendors") . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $subdir)) {
+                                $sourcePath = $packageDir . DIRECTORY_SEPARATOR . "assets";
+                                $publicPath = public_path("vendors/" . $dir . DIRECTORY_SEPARATOR . $subdir);
+                                File::makeDirectory($publicPath, 493, true, true);
+                                File::link($sourcePath, $publicPath);
+                            }
+
+                            // scan migration
+                            if (is_dir($packageDir . DIRECTORY_SEPARATOR . "database/migrations")) {
+                                $migrationPath = $packageDir . DIRECTORY_SEPARATOR . "database/migrations";
+                                $meta["migrationPath"] = $migrationPath;
+
+                                $migrationPaths[] = $migrationPath;
+                            }
+
+                            $metas[$subdir] = $meta;
+
+                            // get value from meta file
+                            $metaProvider = array_key_exists("providers", $meta) ? $meta["providers"] : [];
+                            $metaAliases = array_key_exists("aliases", $meta) ? $meta["aliases"] : [];
+
+                            $providers = array_merge($providers, $metaProvider);
+                            $aliases = array_merge($aliases, $metaAliases);
+                        }
+
+                    }
+                }
+            }
+
+            Eventy::addFilter("meta-packages", function ($old) use ($metas) {
+                if (!is_array($old)) {
+                    $old = [];
+                }
+
+                return array_merge($old, $metas);
+            });
+
+            // set providers
+            foreach ($providers as $provider) {
+                $this->app->register($provider);
+            }
+
+            // set aliases
+            foreach ($aliases as $alias) {
+                $this->app->alias($alias[1], $alias[0]);
+            }
+
+            // set migration path
+            $migrationPaths[] = "database/migrations";
+            $this->loadMigrationsFrom($migrationPaths);
+        }catch (\Exception $exception){
+            printf("packages directory not found \n");
+        }
+
+    }
+
+    private function updateComposerAutoload()
+    {
+        $composerFile = base_path("composer.json");
+        $reader = new ComposerReader($composerFile);
+        $section = new AutoloadSection($reader, AutoloadSection::TYPE_PSR4);
+        $packages = $this->getPackages();
+        $composerPackages = [];
+        foreach ($section as $autoload) {
+            $namespace = Str::replaceFirst("\\", "/", $autoload->namespace);
+            $composerPackages[] = Str::replaceFirst("\\", "", $namespace);
+        }
+
+        $unavailableYet = array_diff(array_keys($packages), $composerPackages);
+
+        if (count($unavailableYet) > 0) {
+            foreach ($unavailableYet as $k => $v) {
+                $namespace = Str::replaceFirst("/", "\\", $v) . "\\";
+                $new = new Autoload($reader, $namespace, $packages[$v] . DIRECTORY_SEPARATOR . "src", AutoloadSection::TYPE_PSR4);
+                $x = new AutoloadSection($reader);
+                $x->add($new)->save();
+            }
+        }
+    }
+
+    private function getPackages()
+    {
+        $container = [];
+        $base = base_path("packages");
+        $exclude = [".", ".."];
+        try{
+            $dirs = scandir($base);
+            foreach ($dirs as $dir) {
+                if (!in_array($dir, $exclude)) {
+                    $subDirs = scandir($base . DIRECTORY_SEPARATOR . $dir);
+                    foreach ($subDirs as $subdir) {
+                        if (!in_array($subdir, $exclude)) {
+                            $container[Str::ucfirst($dir) . DIRECTORY_SEPARATOR . Str::ucfirst($subdir)] = "packages/" . $dir . DIRECTORY_SEPARATOR . $subdir;
+                        }
+                    }
+                }
+            }
+
+            return $container;
+        }catch (\Exception $exception){
+            return [];
+        }
+
+    }
+}
